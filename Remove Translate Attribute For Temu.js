733a88b1405e27f166fe0bc557c9a3d2406c1841
胡硕德 <52638884+KPI0@users.noreply.github.com>
2024-04-02 17:47:23 +0800

    Update Remove Translate Attribute For Temu.js

--- Remove Translate Attribute For Temu.js
+++ Remove Translate Attribute For Temu.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name         Remove Translate Attribute For Temu
-// @namespace    https://github.com/KPI0/tampermonkey
-// @version      0.1
-// @description  Remove translate attribute from HTML tag
-// @author       KPI0
-// @match        https://www.temu.com/*
-// @grant        none
+// @name                Remove Translate Attribute For Temu
+// @name:zh-CN          Temu去除翻译限制
+// @namespace           https://github.com/KPI0/tampermonkey
+// @version             0.2
+// @description         Remove translate attribute from HTML tag
+// @description:zh-cn   Temu购物平台限制了网页翻译，通过此脚本可去除限制
+// @author              KPI0
+// @match               https://www.temu.com/*
+// @grant               none
 // ==/UserScript==
 
 (function() {
